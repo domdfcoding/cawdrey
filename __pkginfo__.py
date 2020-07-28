f73059977151e47ad16689c47a292038ef1abc08
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-07-28 08:53:59 +0100

    Bump version: 0.1.6 → 0.1.7

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -39,7 +39,7 @@ __copyright__ = """
 2019-2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
 """
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 modname = "Cawdrey"
 pypi_name = "cawdrey"
 import_name = "cawdrey"
