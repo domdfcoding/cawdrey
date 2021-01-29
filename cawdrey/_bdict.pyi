bc399e2b3ed85e23420bd41ba3a957a0809df064
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-01-29 22:25:11 +0000

    Linting stubs

--- cawdrey/_bdict.pyi
+++ cawdrey/_bdict.pyi
@@ -5,7 +5,6 @@ from typing import Iterable, Optional
 # this package
 from cawdrey.base import KT, VT
 
-
 class bdict(UserDict[KT, VT]):
 
 	def __init__(self, seq: Optional[Iterable] = ..., **kwargs): ...
