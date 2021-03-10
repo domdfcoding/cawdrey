67e98a61b982869715621c7e69f61595ef782d79
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-03-10 07:59:00 +0000

    Bump version v0.4.0 -> v0.4.1

--- cawdrey/__init__.py
+++ cawdrey/__init__.py
@@ -38,7 +38,7 @@ from .tally import Tally
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020 Dominic Davis-Foster"
 __license__: str = "LGPLv3+"
-__version__: str = "0.4.0"
+__version__: str = "0.4.1"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = [
